e205ae33813faca45f79e2f6f0c97dfe9f1ea33f
archie.cobbs <archie.cobbs@9ebbb9bc-6d00-c93f-51fe-0c3cf73ff30a>
2013-05-17 22:09:13 +0000

    Man pages.

---
+++ wiki/regen-manpages.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Sanity check
+if [ `uname -s` = 'Darwin' ]; then
+    echo "This doesn't work on Mac OS" 1>&2
+    exit 1
+fi
+
+for MANPAGE in sendsms smslen twilog; do
+
+    if [ ! -f ../trunk/"${MANPAGE}".1 ]; then
+        echo "../trunk/${MANPAGE}.1 not found" 1>&2
+        exit 1
+    fi
+
+    printf '#summary Wikified version of the %s man page\n#labels Featured\n\n{{{\n' "${MANPAGE}" > ManPage-"${MANPAGE}".wiki
+    groff -r LL=100n -r LT=100n -Tlatin1 -man ../trunk/"${MANPAGE}".1 | sed -r -e 's/.\x08(.)/\1/g' -e 's/[[0-9]+m//g' >> ManPage-"${MANPAGE}".wiki
+    echo '}}}' >> ManPage-"${MANPAGE}".wiki
+
+done
