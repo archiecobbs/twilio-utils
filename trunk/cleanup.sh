e6ddb82e6f0bc240404ac7d235c7447c7d8da9b3
archie.cobbs <archie.cobbs@9ebbb9bc-6d00-c93f-51fe-0c3cf73ff30a>
2013-05-17 21:40:09 +0000

    Initial checkin.

---
+++ trunk/cleanup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# $Id$
+
+#
+# Script to clean out generated GNU auto* gunk.
+#
+
+set -e
+
+echo "cleaning up"
+rm -rf autom4te*.cache scripts aclocal.m4 configure config.log config.status .deps stamp-h1 a.out.dSYM
+rm -f config.h.in config.h.in~ config.h
+rm -f scripts
+find . \( -name Makefile -o -name Makefile.in \) -print0 | xargs -0 rm -f
+rm -f svnrev.c
+rm -f *.o smslen
+rm -f twilio-utils-?.?.?.tar.gz
+rm -f sendsms{,.1}
+rm -f smslen.1
+rm -f twilog{,.1}
